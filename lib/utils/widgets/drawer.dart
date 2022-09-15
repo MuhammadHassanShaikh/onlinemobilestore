f238919aa5fc2dd3888b6ca7622e11b7a1250d5d
mh0848128 <mh0848128@gmail.com>
2022-09-15 13:20:04 -0700

    hi

--- lib/utils/widgets/drawer.dart
+++ lib/utils/widgets/drawer.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MyDrawer extends StatelessWidget {
@@ -6,7 +7,7 @@ class MyDrawer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final imageUrl =
-        "https://www.123rf.com/photo_47332506_programming-work-time-programmer-typing-new-lines-of-html-code-laptop-and-hand-closeup-working-time-.html";
+        "https://previews.123rf.com/images/welcomia/welcomia1510/welcomia151000045/47332506-programming-work-time-programmer-typing-new-lines-of-html-code-laptop-and-hand-closeup-working-time-.jpg";
     return Drawer(
       child: Container(
         color: Colors.deepPurple,
@@ -24,7 +25,39 @@ class MyDrawer extends StatelessWidget {
                 ),
               ),
             ),
-            
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.home,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Home",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.profile_circled,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Profile",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            ListTile(
+              leading: Icon(
+                CupertinoIcons.mail,
+                color: Colors.white,
+              ),
+              title: Text(
+                "Email me",
+                textScaleFactor: 1.2,
+                style: TextStyle(color: Colors.white),
+              ),
+            )
           ],
         ),
       ),
